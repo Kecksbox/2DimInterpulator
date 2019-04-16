29931bd1eb3ad89ec0fc857499b73124edaaf20c
Kecksbox <malte_heuser@web.de>
2019-04-16 20:46:35 +0200

    backup

--- dist/lib/datatypes/Grid.js
+++ dist/lib/datatypes/Grid.js
@@ -5,6 +5,12 @@ class Grid {
         this.data = data;
     }
     add(dataPoint) {
+        for (let i = 0, len = this.data.length; i < len; ++i) {
+            if (this.data[i].x === dataPoint.x && this.data[i].y === dataPoint.y) {
+                this.data[i].z = dataPoint.z;
+                return;
+            }
+        }
         this.data.push(dataPoint);
     }
     remove(dataPoint) {
